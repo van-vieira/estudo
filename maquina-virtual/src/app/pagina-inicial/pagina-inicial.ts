d38137b5078131e5fea743112b8c89d27da7ab65
van-vieira <vm-vanessa@ETV814621.d02.prodemge.gov.br>
2025-10-06 12:35:08 -0300

    criaçao da primeira pagina

---
+++ maquina-virtual/src/app/pagina-inicial/pagina-inicial.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-inicial',
+  imports: [],
+  templateUrl: './pagina-inicial.html',
+  styleUrl: './pagina-inicial.scss'
+})
+export class PaginaInicial {
+
+}
